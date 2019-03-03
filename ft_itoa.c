640efb6d7a61e261667bccd7c1117ebe74456fdd
Allan Mamy <amamy@student.42.fr>
2019-03-03 19:29:57 +0100

    add itoa_base, edit itoa and ft_bn_len_base, norm lstenadd

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,28 +6,11 @@
 /*   By: amamy <amamy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/22 14:03:39 by amamy             #+#    #+#             */
-/*   Updated: 2019/02/07 20:15:41 by amamy            ###   ########.fr       */
+/*   Updated: 2019/03/03 18:03:28 by amamy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int		nb_size(int i)
-{
-	size_t nb_size;
-
-	nb_size = 0;
-	if (i == 0 || i == -0)
-		nb_size = 1;
-	if (i < 0)
-		nb_size++;
-	while (i != 0)
-	{
-		i = i / 10;
-		nb_size++;
-	}
-	return (nb_size);
-}
 
 static char		*ft_nb_neg(char *str, int n)
 {
@@ -36,10 +19,10 @@ static char		*ft_nb_neg(char *str, int n)
 
 	counter = 0;
 	str[counter] = '-';
-	end = nb_size(n);
+	end = ft_nb_len_base(n, 10);
 	if (n == -2147483648)
 	{
-		counter = nb_size(n) - 1;
+		counter = ft_nb_len_base(n, 10) - 1;
 		str[counter] = (n % 10) * -1 + 48;
 		n = n / 10;
 		counter--;
@@ -47,7 +30,7 @@ static char		*ft_nb_neg(char *str, int n)
 	n = -n;
 	str[end] = '\0';
 	if (n != -2147483648)
-		counter = nb_size(n);
+		counter = ft_nb_len_base(n, 10);
 	while (n != 0)
 	{
 		str[counter] = (n % 10) + 48;
@@ -63,12 +46,12 @@ char			*ft_itoa(int n)
 	size_t	counter;
 
 	counter = 0;
-	if (!(str = (char*)malloc(sizeof(char) * nb_size(n) + 1)))
+	if (!(str = (char*)malloc(sizeof(char) * ft_nb_len_base(n, 10) + 1)))
 		return (NULL);
 	if (n == 0 || n == -0)
-		str[0] = 48;
+		str[0] = '0';
 	if (n > 0)
-		counter = nb_size(n) - 1;
+		counter = ft_nb_len_base(n, 10) - 1;
 	if (n < 0)
 		return (ft_nb_neg(str, n));
 	str[counter + 1] = '\0';
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: amamy <amamy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/10 20:20:26 by amamy             #+#    #+#             */
-/*   Updated: 2019/03/03 15:44:34 by amamy            ###   ########.fr       */
+/*   Updated: 2019/03/03 18:26:01 by amamy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,5 +90,6 @@ char			*ft_strrealloc(char *str, int len);
 char			*ft_strndup(const char *s1, ssize_t len);
 void			lstendadd(t_list *alst, t_list *new);
 int				ft_nb_len_base(int value, int base);
+char			*ft_itoa_base(int n, int base);
 
 #endif
