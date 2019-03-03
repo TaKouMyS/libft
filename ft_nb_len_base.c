d7088eaabfee924156c28fffbdc84b7f07ae49e6
Allan Mamy <amamy@student.42.fr>
2019-03-03 15:53:23 +0100

    add ft_nb_len_base.c and some makefile header  MAJ

---
+++ ft_nb_len_base.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_nb_len_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amamy <amamy@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/03 14:38:37 by amamy             #+#    #+#             */
+/*   Updated: 2019/03/03 15:51:49 by amamy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Takes an int and tells you how much space it would take if the number
+** were in base X;
+*/
+
+int	ft_nb_len_base(int value, int base)
+{
+	int	size;
+
+	size = 0;
+	if (value < 0)
+		size++;
+	while (value != 0)
+	{
+		value = value / base;
+		size++;
+	}
+	return (size);
+}
